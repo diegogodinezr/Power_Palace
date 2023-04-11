7e53ce6e00f6e110392a13e3b8849feea6361032
Danielhndzfor <danielhndzfor@hotmail.com>
2023-04-11 09:54:21 -0600

    home y login modificado

--- membresias/urls.py
+++ membresias/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     url(r'^$',home, name="home"),
     path('membresias/', login_usuario, name="login_usuario"),
+    path('login_usuario/', login_usuario, name="login_usuario"),
     path('verificacionID/', verificacionID, name="verificacionID"),
     path('logout/',LogoutView.as_view(next_page='/'), name='logout'),
 ]
