2e480b335a160475af54539de66dcf9eac22b249
Danielhndzfor <danielhndzfor@hotmail.com>
2023-05-01 15:33:51 -0600

    obteniendo actualizaciones

--- ventas/models.py
+++ ventas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class producto(models.Model):
     id = models.AutoField(primary_key=True) # Campo para el ID del producto, con auto-incremento
-    id_producto = models.CharField(max_length=20)
+    id_producto = models.IntegerField(max_length=20)
     nombre = models.CharField(max_length=20)
     precio = models.FloatField()
     descripcion = models.CharField(max_length=200)
