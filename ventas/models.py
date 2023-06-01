947da4bb20c9ebda3368caf02dc8a26412358a5a
Danielhndzfor <20460185@colima.tecnm.mx>
2023-06-01 15:40:18 -0600

    back del Ventas

--- ventas/models.py
+++ ventas/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class producto(models.Model):
+class Producto(models.Model):
     id = models.AutoField(primary_key=True) # Campo para el ID del producto, con auto-incremento
-    id_producto = models.IntegerField()
+    id_producto = models.CharField(max_length=20,unique=True)
     nombre = models.CharField(max_length=20)
     precio = models.FloatField()
     descripcion = models.CharField(max_length=200)
     cantidad = models.IntegerField()
     categoria = models.CharField(max_length=70)   
 
+class Venta(models.Model):
+    id_producto = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
+    precio = models.FloatField()
+    cantidad = models.IntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    # Agrega más campos según tus necesidades
+
+    def __str__(self):
+        return f"Venta {self.id}"
